1fa7e967b57ec83e0258bb65c2a8febb40eda577
pedjo95 <tec.ped95@gmail.com>
2020-06-08 23:02:46 -0300

    get and display post

--- src/app.js
+++ src/app.js
@@ -12,3 +12,15 @@
 
 // import greeting from "./module2";
 // console.log(greeting);
+
+import { http } from "./http";
+import { ui } from "./ui";
+
+document.addEventListener("DOMContentLoaded", getPosts);
+
+function getPosts() {
+  http
+    .get("http://localhost:3000/post")
+    .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err));
+}
